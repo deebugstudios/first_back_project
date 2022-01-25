ef79bd3baf81898984c82402b9dc7c08d0795589
deebugstudios <deebugstudios@gmail.com>
2022-01-25 10:26:40 +0100

    Variables, data types and comments

--- first.js
+++ first.js
@@ -1,1 +1,20 @@
-console.log('Hello Deebuggers!!!');
+console.log('Good day everyone')
+console.log('Class is going great')
+console.log("John Mote")
+console.log(45)
+console.log(45 - 10)
+console.log('45 - 10')
+
+// Comments
+// Single line comment
+console.log('Hello people'); // this kind of comment can come after your code
+
+// this is a comment
+// This is another comment
+
+/**
+ * This is a multi line comment
+ * it can extend over several lines
+ * without you needing to do anything 
+ * extra
+ */
---
+++ variables_datatypes.js
@@ -0,0 +1,19 @@
+// Variables and Data types
+
+let age;
+age = 25;
+console.log(age);
+
+let name = 'John';
+console.log(name);
+
+let finalResult; // undefined
+console.log(finalResult);
+let fnum = 5, snum = 7;
+let sum = fnum + snum;
+console.log(sum);
+
+let firstN = 10, secondN = 6;
+finalResult = firstN + secondN * sum;
+console.log(finalResult);
+
